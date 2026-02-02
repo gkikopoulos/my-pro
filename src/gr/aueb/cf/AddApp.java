abe0863abc359941a7f989cb1e8e613544a11230
gkikopoulos <gkikopoulos.ioannis@gmail.com>
2026-02-03 01:03:58 +0200

    fix: add print formatted

--- src/gr/aueb/cf/AddApp.java
+++ src/gr/aueb/cf/AddApp.java
@@ -4,6 +4,7 @@ public class AddApp {
 
     public static void main(String[] args) {
         System.out.println(add(2, 3));
+        System.out.printf("%d", add(3, 5));
     }
 
     public static int add(int a, int b) {
